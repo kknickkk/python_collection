14eb2f5ab8c15552261bb232a60e6f82948161a6
kknickkk <Nick@MacBook-Pro-di-Nick.local>
2017-04-08 19:49:21 +0200

    stream_youtube.py working now

--- stream_youtube.py
+++ stream_youtube.py
@@ -1,18 +1,35 @@
-#!/usr/bin/python3
-
-from subprocess import call
+from subprocess import call, check_output
+import urllib
+import urllib2
+from sys import argv
+from bs4 import BeautifulSoup
 
 def main():
 
 	title = argv[1]
 
 	url = search(title)
-
+	print(url)
 	play(url)
 
 def play(url):
-	string = 'mpv (youtube-dl -f140 -g ' + url +')'
-def search(title):
 
-	
-	
+	string = 'youtube-dl -f140 -g ' + url 
+	y = check_output(string.split())
+	p = 'mpv ' + y
+	call(p.split())
+
+def search(title):
+	query = urllib.quote(title)
+	url = "https://www.youtube.com/results?search_query=" + query
+	response = urllib2.urlopen(url)
+	html = response.read()
+	soup = BeautifulSoup(html)
+	for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+	    break
+
+	return ('https://www.youtube.com' + vid['href'])
+
+
+if __name__ == '__main__':
+	main()
