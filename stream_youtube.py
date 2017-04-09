e212a87535c830d481eb11738a1ed344c29e4ea3
kknickkk <me@ngdurante.eu>
2017-04-09 09:54:56 +0200

    moved to python3

--- stream_youtube.py
+++ stream_youtube.py
@@ -1,14 +1,13 @@
 from subprocess import call, check_output
-import urllib
-import urllib2
+import urllib.parse
+import urllib.request
 from sys import argv
 from bs4 import BeautifulSoup
 
 def main():
 
-	#title = argv[1]
 	while True:
-		title = str(raw_input('Enter song:\n'))
+		title = input('Enter song:\n')
 		url = search(title + ' HQ')
 		play(url)
 	
@@ -18,16 +17,16 @@ def play(url):
 
 	string = 'youtube-dl -f140 -g ' + url 
 	y = check_output(string.split())
-	p = 'mpv ' + y
+	p = 'mpv ' + y.decode()
 	call(p.split())
 
 def search(title):
 
-	query = urllib.quote(title)
+	query = urllib.parse.quote(title)
 	url = "https://www.youtube.com/results?search_query=" + query
-	response = urllib2.urlopen(url)
+	response = urllib.request.urlopen(url)
 	html = response.read()
-	soup = BeautifulSoup(html)
+	soup = BeautifulSoup(html, "html.parser")
 	for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
 	    break
 	return ('https://www.youtube.com' + vid['href'])
